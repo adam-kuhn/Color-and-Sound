87e4da041500e47ae6a631e87d73a7679a3ff10e
adam-kuhn <adam.kuhn20@gmail.com>
2017-12-02 11:02:42 +1300

    link css to index and default boxes to white

--- js/main-java.js
+++ js/main-java.js
@@ -1,11 +1,16 @@
+//get element by Class then change color based on selection
+//display no text when selected, only the color
+//play button will search for class then assign a sound 3-5 sec?
 
 //functions to be loaded on start up
 window.onload = function (){
+
+
 document.getElementById("grid").addEventListener(click, chooseColor);
 
 }
 
 //drop down of colors to select
 function chooseColor(){
-  
+
 }
