da46a44bf7c070b9605b38e7b7e273e04348abf5
adam-kuhn <adam.kuhn20@gmail.com>
2017-12-02 10:37:57 +1300

    add event listener to grid

---
+++ js/main-java.js
@@ -0,0 +1,11 @@
+
+//functions to be loaded on start up
+window.onload = function (){
+document.getElementById("grid").addEventListener(click, chooseColor);
+
+}
+
+//drop down of colors to select
+function chooseColor(){
+  
+}
