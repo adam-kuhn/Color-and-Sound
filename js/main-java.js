64db1d03735c9b69426ddbc1e9a446c01c188240
adam-kuhn <adam.kuhn20@gmail.com>
2017-12-02 23:05:29 +1300

    confirmed that event Listeners work

--- js/main-java.js
+++ js/main-java.js
@@ -3,24 +3,44 @@
 //play button will search for class then assign a sound 3-5 sec?
 
 //functions to be loaded on start up
-window.onload = function (){
+window.onload = function(){
+var colorBox = document.getElementsByClassName('colorBox')
 
-chooseColor()
-
+for (var i = 0; i < colorBox.length; i++) {
+  colorBox[i].addEventListener('click', makeBlue)
 }
 
+function makeBlue (evt) {
+  evt.target.style.backgroundColor = 'blue'
+}
+}
+/*window.onload = function (){
+  var colorBox = document.getElementsByClassName('colorBox')
+  for (var i = 0; i < colorBox.length; i ++){
+    colorBox[i].addEventListener('click', chooseColor(evt))
+  }
+
+
+
+}
+let evt = "x"
+function chooseColor(evt){
+
+    evt.target.style.backgroundColor = 'red'
+
+}
 //drop down of colors to select
-function chooseColor(){
 
-  $('.select').change(function(){
-    $(this).find('option:selected').css('background-color', 'red');
-  })
 
-  /*let chosenColor = document.getElementsByClassName("color-box")
+/*function chooseColor(evt){
+evt.target.classList.toggle('blue')
+
+
+  /*let chosenColor = document.getElementsByClassName("colorBox")
   for (i = 0; i < chosenColor.length; i++){
     if (chosenColor[i] == "white")
     return "yes"
   }
   console.log(chosenColor)*/
 
-}
+//}
