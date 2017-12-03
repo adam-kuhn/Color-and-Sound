3cdbf94080ae699ca6bda2e62bd2d122f8b8256c
adam-kuhn <adam.kuhn20@gmail.com>
2017-12-03 14:54:20 +1300

    add counter to count clicks of each indivdual button

--- js/main-java.js
+++ js/main-java.js
@@ -7,9 +7,14 @@
 window.onload = function (){
   var colorBox = document.getElementsByClassName('colorBox')
   for (var i = 0; i < colorBox.length; i ++){
-    colorBox[i].addEventListener('click', changeColor)
+    colorBox[i].addEventListener('click', click)
+
+    /*document.getElementById("box1").addEventListener('click', click)
+    document.getElementById("box2").addEventListener('click', click)
+    document.getElementById("box3").addEventListener('click', click)*/
 
   }
+}
 
 /*  var colorBox = document.getElementsByClassName('colorBox')
   for (var i = 0; i < colorBox.length; i ++){
@@ -23,15 +28,68 @@ window.onload = function (){
     colorBox[i].addEventListener('click', makeYellow)
   } */
 
+/*var box1 = document.getElementById("box1"),
+    box2 = document.getElementById("box2"),
+    box3 = document.getElementById("box3"),*/
+    clicks = {};
 
+function click(e) {
+  var id = e.target.id;
+  if (!clicks[id]){
+    clicks[id] = 0;
+  }
+  else if (clicks[id] === 5){
+    clicks[id] = 0
+  }
+
+  clicks[id]++;
+  e.target.textContent = clicks[id]
+  console.log(clicks[id])
 }
 
 
 
 
-let clicks = 0;
 
+
+
+/*  let clicks = 0;
 function changeColor(evt){
+
+  clicks += 1
+
+  switch(clicks) {
+    case 1:
+    evt.target.classList.toggle('white');
+    break;
+
+    case 2:
+    evt.target.classList.toggle('white');
+    evt.target.classList.toggle('blue');
+    break;
+
+    case 3:
+    evt.target.classList.toggle('blue');
+    evt.target.classList.toggle('green');
+    break;
+
+    case 4:
+    evt.target.classList.toggle('green');
+    evt.target.classList.toggle('yellow');
+    break;
+
+    case 5:
+    clicks = 0;
+    evt.target.classList.toggle('yellow')
+
+
+
+  }
+  console.log(clicks)
+}
+
+function changeColor2(evt){
+  //let clicks = 0;
   clicks += 1
 
   switch(clicks) {
