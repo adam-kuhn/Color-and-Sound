4c3d48bcb9c10cd9ba8889db07f1350922605307
adam-kuhn <adam.kuhn20@gmail.com>
2017-12-03 14:03:26 +1300

    resize button add text rmv if then with a switch

--- js/main-java.js
+++ js/main-java.js
@@ -7,7 +7,7 @@
 window.onload = function (){
   var colorBox = document.getElementsByClassName('colorBox')
   for (var i = 0; i < colorBox.length; i ++){
-    colorBox[i].addEventListener('click', clickCount)
+    colorBox[i].addEventListener('click', changeColor)
 
   }
 
@@ -25,14 +25,64 @@ window.onload = function (){
 
 
 }
-var clicks = 0;
-function clickCount() {
+
+
+
+
+let clicks = 0;
+
+function changeColor(evt){
   clicks += 1
-  console.log(clicks)
-  if (clicks === 11){
-    clicks = 0
+
+  switch(clicks) {
+    case 1:
+    evt.target.classList.toggle('white');
+    break;
+
+    case 2:
+    evt.target.classList.toggle('white');
+    evt.target.classList.toggle('blue');
+    break;
+
+    case 3:
+    evt.target.classList.toggle('blue');
+    evt.target.classList.toggle('green');
+    break;
+
+    case 4:
+    evt.target.classList.toggle('green');
+    evt.target.classList.toggle('yellow');
+    break;
+
+    case 5:
+    clicks = 0;
+    evt.target.classList.toggle('yellow')
+
+
+
   }
+  console.log(clicks)
 }
+/* using if then
+  if (clicks === 5){
+  clicks = 0
+  evt.target.classList.toggle('yellow')
+  }
+  if (clicks === 1){
+    //evt.target.classList.toggle('yellow')
+    evt.target.classList.toggle('white')
+  } else if (clicks === 2){
+    evt.target.classList.toggle('white')
+    evt.target.classList.toggle('blue')
+  } else if (clicks === 3){
+    evt.target.classList.toggle('blue')
+    evt.target.classList.toggle('green')
+  } else if (clicks === 4){
+    evt.target.classList.toggle('green')
+    evt.target.classList.toggle('yellow')
+  }
+} /*
+
 /*function clickCount(evt){
   var counter = evt.currentTarget;
   counter.clicks = (counter.clicks || 0) + 1;
